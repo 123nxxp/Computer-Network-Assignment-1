6936573eb2a2f242e85313f1888f9832960a1f17
Mahesh Dange <105475941+123nxxp@users.noreply.github.com>
2023-09-11 01:27:39 +0530

    Update part1.c

--- part1.c
+++ part1.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Structure to represent a unique TCP connection
+
 struct TCPConnection {
     char source_ip[INET_ADDRSTRLEN];
     char destination_ip[INET_ADDRSTRLEN];
@@ -13,7 +13,7 @@ struct TCPConnection {
     uint16_t destination_port;
 };
 
-// Compare function for TCP connections
+
 int compare_connection(const void *a, const void *b) {
     const struct TCPConnection *conn_a = (const struct TCPConnection *)a;
     const struct TCPConnection *conn_b = (const struct TCPConnection *)b;
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    struct TCPConnection connection_list[1024];  // Assuming a maximum of 1024 unique connections
+    struct TCPConnection connection_list[1024];  
     int connection_count = 0;
 
     while (true) {
@@ -55,14 +55,14 @@ int main(int argc, char *argv[]) {
         struct ip *ip_packet_header = (struct ip *)(packet_data + 14);
         struct tcphdr *tcp_packet_header = (struct tcphdr *)(packet_data + 14 + ip_packet_header->ip_hl * 4);
 
-        // Extract the connection
+       
         struct TCPConnection current_connection;
         inet_ntop(AF_INET, &(ip_packet_header->ip_src), current_connection.source_ip, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(ip_packet_header->ip_dst), current_connection.destination_ip, INET_ADDRSTRLEN);
         current_connection.source_port = ntohs(tcp_packet_header->th_sport);
         current_connection.destination_port = ntohs(tcp_packet_header->th_dport);
 
-        // Check if the connection is already in the list
+        
         bool connection_exists = false;
         for (int i = 0; i < connection_count; i++) {
             if (compare_connection(&connection_list[i], &current_connection) == 0) {
@@ -72,11 +72,11 @@ int main(int argc, char *argv[]) {
         }
 
         if (!connection_exists) {
-            // Add the new connection to the list
+            
             connection_list[connection_count] = current_connection;
             connection_count++;
 
-            // Print the connection information
+            // Print
             printf("Source IP: %s, Source Port: %d, Destination IP: %s, Destination Port: %d, Connection Count: %d\n",
                    current_connection.source_ip, current_connection.source_port,
                    current_connection.destination_ip, current_connection.destination_port, connection_count);
